3a31563850b0ccea23885989bc22eda34c9e3ce1
Anjoom75 <anjoomnafis.2@gmail.com>
2026-05-21 00:04:16 +0600

    chapter5

--- first_progamme.c
+++ first_progamme.c
@@ -2,9 +2,11 @@
 
 int main(){
     //printf("nah did tthis before \n not my firrst programme");
-    printf("ayo \"jar of flies\" or dirt ?");
-    printf(" \a alert");
-    
+    //printf("ayo \"jar of flies\" or dirt ?");
+    //printf(" \a alert");
+    int broh = 3;
+    //printf("hesitaion is defeat <%d",broh);
+    const float pi = 3.1416;
 
 
     return 0;
---
+++ star.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    printf("*\n**\n***\n****\n*****\n");
+    return 0;
+}
+
+//yea not a great wAy to do this '_'
