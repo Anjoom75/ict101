804b26199982f087b510dd99c23d4bdbbe4dc878
Anjoom75 <anjoomnafis.2@gmail.com>
2026-05-20 23:42:13 +0600

    chapter5

---
+++ first_progamme.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  // header file
+
+int main(){
+    //printf("nah did tthis before \n not my firrst programme");
+    printf("ayo \"jar of flies\" or dirt ?");
+    printf(" \a alert");
+    
+
+
+    return 0;
+}
